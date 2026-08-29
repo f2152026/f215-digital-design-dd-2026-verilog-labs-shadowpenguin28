440e86b424c981cec2735643efb4ab06315f18c1
Ishan Kumar <67703117+shadowpenguin28@users.noreply.github.com>
2026-08-29 10:34:42 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -16,7 +16,7 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  and #(2) (pc2, cin, ps);
+  and #(2) (pc2, cin, ps); // movied and gate to the top, no change
   xor #(2) (ps,  a,   b);
   and #(2) (pc1, a,   b);
   xor #(2) (sum, cin, ps);
